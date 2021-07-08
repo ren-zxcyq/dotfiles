96a61d8e9453e741f65ee5a5917d1453af649612
ren-zxcyq <62415460+ren-zxcyq@users.noreply.github.com>
2021-07-08 01:00:39 +0100

    Delete install_zsh.sh
    Moved contents to setup.sh

--- install_zsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install oh-my-zsh
-# Install normally
-#sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#Unattended install
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended \
-# Make zsh your default shell
-chsh -s $(which zsh)
